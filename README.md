c9a3d737008d3c610e8895cbcfaaeceb418f4310
AndreyGeonya <a.geonya@gmail.com>
2014-09-04 19:38:35 +0300

    refactoring

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Hull.js - JavaScript library that builds concave hulls by set of points.
+
+https://github.com/tmcw/k-means
+http://people.mpi-inf.mpg.de/~jgiesen/tch/sem06/Celikik.pdf
--- src/hull.js
+++ src/hull.js
@@ -1,65 +1,60 @@
 (function() {
 
-function _dsq(a, b) {
-    var dx = a[0] - b[0], dy = a[1] - b[1];
+function _squaredDist(px1, px2) {
+    var dx = px1[0] - px2[0],
+        dy = px1[1] - px2[1];
     return dx * dx + dy * dy;
 }
 
 // http://stackoverflow.com/questions/14108553/get-border-edges-of-mesh-in-winding-order#comment19522625_14108553
-// TODO: construct multipolygons before sorting. Think about clusters.
 // TODO: think about O(N^2) optimization
-function _edges2Border(edges) {
+function _edges2ccwPoly(edges) {
     var checked = {},
-        border = [],
+        poly = [],
         edge = edges[0],
         nextEdge,
-        j = 0;
+        j = 0,
+        maxJ = Math.pow(edges.length, 2);
 
     checked[edge[0] + '-' + edge[1]] = true;
-    border.push(edge[1]);
+    poly.push(edge[1]);
     
-    while (border.length !== edges.length) {
-        // templary infinity loop break for multipolygons
-        if (j > Math.pow(edges.length, 2)) {
-            break;
+    while (poly.length !== edges.length) {
+        if (j > maxJ) {
+            break; // stop infinity loop for multipolygons
         }
         for (var i = 0; i < edges.length; i++) {
-            j++;
             var nextEdge = edges[i],
                 k = nextEdge[0] + '-' + nextEdge[1];
-
             if (checked[k] === true) {
                 continue;
             }
-
             if (nextEdge[0] === edge[1] || nextEdge[1] === edge[1]) {
                 if (nextEdge[0] === edge[1]) {
-                    border.push(nextEdge[1]);
+                    poly.push(nextEdge[1]);
                 }
                 if (nextEdge[1] === edge[1]) {
-                    border.push(nextEdge[0]);
+                    poly.push(nextEdge[0]);
                 }
                 edge = nextEdge;
                 checked[k] = true;
                 break;
             }
+            j++;
         }
     }
 
-    return border;
+    return poly;
 }
 
-function hull(vertices, alpha) {
-    var tIdxs = Delaunay.triangulate(vertices),
-        trianglesInEdge = {},
-        borderEdges = [],
-        alpha = alpha || 50,
-        asq = alpha * alpha,
-        addKeyToHash = function(key) {
-            trianglesInEdge[key] = trianglesInEdge[key] === undefined ? 1 : trianglesInEdge[key] + 1;
+function _edges2TriCount(pixels, tIdxs, sqTolerance) {
+    var trianglesInEdge = {},
+        incTriCount = function(a, b) {
+            var key = a + '-' + b;
+            trianglesInEdge[key] = trianglesInEdge[key] === undefined ? [1, a, b] : 
+            trianglesInEdge[key][0] + 1;
         };
 
-    // count how many triangles related with each edge
     for (var i = tIdxs.length; i; ) {
         var tIdx = [];
 
@@ -67,84 +62,46 @@ function hull(vertices, alpha) {
         --i; tIdx[1] = tIdxs[i];
         --i; tIdx[2] = tIdxs[i];
 
-        if (_dsq(vertices[tIdx[0]], vertices[tIdx[1]]) < asq &&
-            _dsq(vertices[tIdx[0]], vertices[tIdx[2]]) < asq &&
-            _dsq(vertices[tIdx[1]], vertices[tIdx[2]]) < asq) {
+        if (_squaredDist(pixels[tIdx[0]], pixels[tIdx[1]]) < sqTolerance &&
+            _squaredDist(pixels[tIdx[0]], pixels[tIdx[2]]) < sqTolerance &&
+            _squaredDist(pixels[tIdx[1]], pixels[tIdx[2]]) < sqTolerance) {
 
-            addKeyToHash(tIdx[0] + '-' + tIdx[1]);
-            addKeyToHash(tIdx[1] + '-' + tIdx[0]);
-            addKeyToHash(tIdx[1] + '-' + tIdx[2]);
-            addKeyToHash(tIdx[2] + '-' + tIdx[1]);
-            addKeyToHash(tIdx[2] + '-' + tIdx[0]);
-            addKeyToHash(tIdx[0] + '-' + tIdx[2]);
+            incTriCount(tIdx[0], tIdx[1]);
+            incTriCount(tIdx[1], tIdx[0]);
+            incTriCount(tIdx[1], tIdx[2]);
+            incTriCount(tIdx[2], tIdx[1]);
+            incTriCount(tIdx[2], tIdx[0]);
+            incTriCount(tIdx[0], tIdx[2]);
         }
     }
 
-    // leave only border edges
-    for (edge in trianglesInEdge) {
-        if (trianglesInEdge[edge] === 1) {
-            var pts = edge.split('-');
-            delete trianglesInEdge[pts[1] + '-' + pts[0]];
-            borderEdges.push([parseInt(pts[0]), parseInt(pts[1])]);
-
-            _drawPx(vertices[pts[0]][0], vertices[pts[0]][1], 'red');
-        }
-    }
-
-    _drawTriangles(vertices, asq);
-
-    return _edges2Border(borderEdges);
+    return trianglesInEdge;
 }
 
-function _drawPx(x, y, color) {
-    if (window.hull.DEBUG === false) {
-        return;
-    }
-    var ctx = window.hull.DEBUG;
-    ctx.fillStyle = color;
-    ctx.beginPath();
-    ctx.arc(x, y, 4, 0, 2 * Math.PI, true);
-    ctx.fill();
-    ctx.closePath();
-}
+function _getBoundaryEdges(edges2TriCount) {
+    var boundaryEdges = [];
 
-function _drawTriangles(vertices, asq) {
-    if (window.hull.DEBUG === false) {
-        return;
-    }
-
-    var ctx = window.hull.DEBUG,
-        tIdxs = Delaunay.triangulate(vertices),
-        triangles = [];
-
-    for (var i = tIdxs.length; i; ) {
-        var tIdx = [];
-        --i; tIdx[0] = tIdxs[i];
-        --i; tIdx[1] = tIdxs[i];
-        --i; tIdx[2] = tIdxs[i];
-        if (_dsq(vertices[tIdx[0]], vertices[tIdx[1]]) < asq &&
-            _dsq(vertices[tIdx[0]], vertices[tIdx[2]]) < asq &&
-            _dsq(vertices[tIdx[1]], vertices[tIdx[2]]) < asq) {
-            triangles.push(tIdx);
+    for (edge in edges2TriCount) {
+        if (edges2TriCount[edge][0] === 1) {
+            var pxs = [edges2TriCount[edge][1], edges2TriCount[edge][2]];
+            delete edges2TriCount[pxs[1] + '-' + pxs[0]];
+            boundaryEdges.push([pxs[0], pxs[1]]);
         }
     }
 
-    triangles.forEach(function(i) {
-        var p1X = vertices[i[0]][0],
-            p1Y = vertices[i[0]][1];
-        ctx.moveTo(p1X, p1Y);
-        var p2X = vertices[i[1]][0],
-            p2Y = vertices[i[1]][1];
-        ctx.lineTo(p2X, p2Y);
-        var p3X = vertices[i[2]][0],
-            p3Y = vertices[i[2]][1];
-        ctx.lineTo(p3X, p3Y);
-        ctx.closePath();
-        ctx.stroke();
-    });
+    return boundaryEdges;
+}
+
+function hull(pixels, tolerance) {
+    var tIdxs = Delaunay.triangulate(pixels),
+        tolerance = tolerance || 50,
+        sqTolerance = tolerance * tolerance,
+        edges2TriCount = _edges2TriCount(pixels, tIdxs, sqTolerance),
+        boundaryEdges = _getBoundaryEdges(edges2TriCount);
+
+    return _edges2ccwPoly(boundaryEdges);
 }
 
 window.hull = hull;
-window.hull.DEBUG = false;
 
 })();
