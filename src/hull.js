ed9f69cd225f34e2c073f3ca3dc7c0446590c30a
AndreyGeonya <a.geonya@gmail.com>
2014-10-10 03:02:37 +0300

    add experimental bbox perf improvement

--- src/hull.js
+++ src/hull.js
@@ -98,19 +98,64 @@ function _intersect(edge, pointset) {
     return false;
 }
 
+function _getBBoxAroundEdge(edge) {
+    var minX, maxX, minY, maxY;
+
+    if (edge[0][0] < edge[1][0]) {
+        minX = edge[0][0] - MAX_EDGE_LENGTH;
+        maxX = edge[1][0] + MAX_EDGE_LENGTH;
+    } else {
+        minX = edge[1][0] - MAX_EDGE_LENGTH;
+        maxX = edge[0][0] + MAX_EDGE_LENGTH;        
+    }
+
+    if (edge[0][1] < edge[1][1]) {
+        minY = edge[0][1] - MAX_EDGE_LENGTH;
+        maxY = edge[1][1] + MAX_EDGE_LENGTH;
+    } else {
+        minY = edge[1][1] - MAX_EDGE_LENGTH;
+        maxY = edge[0][1] + MAX_EDGE_LENGTH;
+    }
+
+    return [
+        [minX, minY], // tl
+        [maxX, maxY]  // br
+    ];
+}
+
+function _insideBBox(point, bbox) {
+    return (
+        point[0] > bbox[0][0] &&
+        point[0] < bbox[1][0] &&
+        point[1] > bbox[0][1] &&
+        point[1] < bbox[1][1]);
+}
+
 function _midPointIdx(edge, innerPoints, convex) {
     var point1Idx = null, point2Idx = null,
         angle1Cos = MAX_CONCAVE_ANGLE_COS,
         angle2Cos = MAX_CONCAVE_ANGLE_COS,
-        a1Cos, a2Cos;
+        a1Cos, a2Cos,
+        bbox = _getBBoxAroundEdge(edge);
+
+    window.ctx.beginPath();
+    window.ctx.rect(bbox[0][0], bbox[0][1], bbox[1][0]-bbox[0][0], bbox[1][1]-bbox[0][1]);
+    window.ctx.globalAlpha=0.2;
+    window.ctx.fillStyle = 'yellow';
+    window.ctx.fill();
+    window.ctx.lineWidth = 1;
+    window.ctx.strokeStyle = 'black';
+    window.ctx.stroke();
+    window.ctx.globalAlpha=1;
 
     for (var i = 0; i < innerPoints.length; i++) {
         if (innerPoints[i] === null) { continue; }
+        if (_insideBBox(innerPoints[i], bbox) === false) { continue; }
 
         a1Cos = _cos(edge[0], edge[1], innerPoints[i]);
         a2Cos = _cos(edge[1], edge[0], innerPoints[i]);
 
-       if (a1Cos > MAX_CONCAVE_ANGLE_COS && a2Cos > MAX_CONCAVE_ANGLE_COS) {
+        if (a1Cos > MAX_CONCAVE_ANGLE_COS && a2Cos > MAX_CONCAVE_ANGLE_COS) {
             if (a1Cos > angle1Cos && !_intersect([edge[0], innerPoints[i]], convex)) {
                 angle1Cos = a1Cos;
                 point1Idx = i;
@@ -122,7 +167,6 @@ function _midPointIdx(edge, innerPoints, convex) {
         }
     }
 
-
     return angle1Cos > angle2Cos ? point1Idx : point2Idx;
 }
 
@@ -132,26 +176,35 @@ function _midPointIdx(edge, innerPoints, convex) {
     1.1. splice() complexity O(N), то есть, сложность вставки midPoint-ов сейчас O(N^2 + N^2).
          innerPoints надо вместо удаления просто маркать как удаленные (FIXED).
     1.2. ф-ю рассчета угла (FIXED)
-    1.3. предрассчитать length для всех edges 1 раз
-    1.4. можем ли как-то ограничить область пооиска midPoint-ов?
+    1.3. можем ли как-то ограничить область пооиска midPoint-ов?
+    1.4. упростить метод intersect
   2. Автоматически считать угол и дистанцию
  */
+
+var time = 0;
 
 function _concave(convex, innerPoints) {
     var midPointIdx,
         midPointInserted = false;
 
+
     for (var i = 0; i < convex.length - 1; i++) {
 
         if (_sqLength([convex[i], convex[i + 1]]) <= MAX_SQ_EDGE_LENGTH) { continue; }
 
+        var start = new Date().getTime();
         midPointIdx = _midPointIdx([convex[i], convex[i + 1]], innerPoints, convex);
+        time += ((new Date().getTime()) - start);
+
         if (midPointIdx !== null) {
             convex.splice(i + 1, 0, innerPoints[midPointIdx]);
             innerPoints[midPointIdx] = null; // mark as deleted (it's faster than splice)
             midPointInserted = true;
         }
+        
+
     }
+
 
     if (midPointInserted) {
         return _concave(convex, innerPoints);
@@ -189,8 +242,11 @@ function hull(pointset) {
     concave = _concave(convex, innerPoints);
     console.timeEnd('concave');
 
+    console.log('Tmp calculated time:', time);
+
     return concave;
 }
 
 var MAX_CONCAVE_ANGLE_COS = Math.cos(70 / (180 / Math.PI)); // angle = 70 deg
-var MAX_SQ_EDGE_LENGTH = Math.pow(10, 2); // len = 10 px
+var MAX_EDGE_LENGTH = 10;
+var MAX_SQ_EDGE_LENGTH = Math.pow(MAX_EDGE_LENGTH, 2);
